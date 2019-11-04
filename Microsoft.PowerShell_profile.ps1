0bf10144dcd6ec06ef5caf3256b844c65f1e188b
Wolfgang Manousek <wolfma@microsoft.com>
2019-11-04 17:10:21 +0100

    updated alias-es

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 . $env:Userprofile"\Documents\WindowsPowerShell\CommonProfile.ps1"
 # Load posh-git example profile
 . $env:Userprofile"\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1"
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- Scripts/autoload/SetAlias.ps1
+++ Scripts/autoload/SetAlias.ps1
@@ -3,6 +3,9 @@ Write-Host "SetAlias"
 function Set-WorkDirectory { Set-Location $WORKDIR }
 function Set-GitDirectory { Set-Location $GITDIR }
 function Set-GitCarbonDirectory { Set-Location $GITDIR\carbon }
+function Set-GitCrisAiDirectory { Set-Location $GITDIR\CrisAi }
+function Set-GitFrontDirectory{ Set-Location $GITDIR\front }
+function Set-GitSpeechServicesDirectory{ Set-Location $GITDIR\SoeechServices }
 function Set-GitCarbonDocDirectory { Set-Location $GITDIR\azure-docs-pr\articles\cognitive-services\Speech\SDK\ }
 function Set-OneDriveScriptDirectory { set-location $env:Userprofile\OneDrive\work\PowerShell }
 function Open-PrInCodeflow(
@@ -28,11 +31,13 @@ function Add-PSModulePath(
 
 set-alias work Set-WorkDirectory
 set-alias gitwork Set-GitDirectory
-#set-alias script Set-ScriptDirectory
 set-alias onescript Set-OneDriveScriptDirectory
 set-alias pr Open-PrInCodeflow
 set-alias apm Add-PSModulePath
-set-alias gitc Set-GitCarbonDirectory
-set-alias gita Set-GitCarbonDocDirectory
-set-alias gitd Set-GitCarbonDocDirectory
+set-alias gitr Set-GitDirectory
+set-alias gitc Set-GitCrisAiDirectory
+set-alias gitf Set-GitFrontDirectory
+set-alias gits Set-GitSpeechServicesDirectory
+
+
 
